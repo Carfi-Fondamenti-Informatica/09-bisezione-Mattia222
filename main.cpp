92091d71b357f6527607e8d25f0c1fd14128e5f4
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-20 11:29:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f(float x){
+    return pow(x,2)*cos(x)+1;
+}
+
+
 int main() {
+   float a=0, b=0, x=0, err=0;
+
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a;
+        cin >> b;
+    }while (f(a)*f(b) >= 0);
+
+    do{
+        x=(a+b)/2;
+        if (f(x) == 0){
+            break;
+        }else if (f(a)*f(b) < 0){
+            b=x;
+        }else{
+            a=x;
+        }
+        err = abs((b-a)/2);
+    }while (err >= 1e-6);
+
+    cout << x << endl;
+    cout << f(x) << endl;
    return 0;
 }
